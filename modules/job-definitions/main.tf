bb67b5c7520af5114c73678a62295bbfe45e3f20
pieterdepetter <pieter.de.petter@veb.be>
2024-12-06 13:34:27 +0100

    Update main.tf

--- modules/job-definitions/main.tf
+++ modules/job-definitions/main.tf
@@ -1,69 +1,69 @@
-# resource "aws_batch_job_definition" "batch_job_definition" {
+resource "aws_batch_job_definition" "batch_job_definition" {
 
-#     for_each = var.image_names
-#     name = each.key
+    for_each = var.image_names
+    name = each.key
 
-#     tags = {
-#         Terraform   = true
-#         Owner       = var.owner
-#         Name        = each.key
-#     }  
+    tags = {
+        Terraform   = true
+        Owner       = var.owner
+        Name        = each.key
+    }  
   
-#     type = "container"
-#     container_properties = <<CONTAINER_PROPERTIES
-# {
-#         "image": "${var.account_id}.dkr.ecr.eu-west-1.amazonaws.com/${each.key}",
-#         "executionRoleArn": "${var.execution_role_arn}",
-#         "jobRoleArn": "${var.job_role_arn}",
-#         "vcpus": 1,
-#         "memory": 2048
-#     }
-# CONTAINER_PROPERTIES
-# }
+    type = "container"
+    container_properties = <<CONTAINER_PROPERTIES
+{
+        "image": "${var.account_id}.dkr.ecr.eu-west-1.amazonaws.com/${each.key}",
+        "executionRoleArn": "${var.execution_role_arn}",
+        "jobRoleArn": "${var.job_role_arn}",
+        "vcpus": 1,
+        "memory": 2048
+    }
+CONTAINER_PROPERTIES
+}
 
-# resource "aws_batch_job_definition" "batch_job_definition_on_demand" {
+resource "aws_batch_job_definition" "batch_job_definition_on_demand" {
 
-#     for_each = var.image_names_on_demand
-#     name = "${each.key}-${var.on_demand_suffix}"
+    for_each = var.image_names_on_demand
+    name = "${each.key}-${var.on_demand_suffix}"
 
-#     tags = {
-#         Terraform   = true
-#         Owner       = var.owner
-#         Name        = each.key
-#     }  
+    tags = {
+        Terraform   = true
+        Owner       = var.owner
+        Name        = each.key
+    }  
   
-#     type = "container"
-#     container_properties = <<CONTAINER_PROPERTIES
-# {
-#         "image": "${var.account_id}.dkr.ecr.eu-west-1.amazonaws.com/${each.key}",
-#         "executionRoleArn": "${var.execution_role_arn}",
-#         "jobRoleArn": "${var.job_role_arn}",
-#         "vcpus": 24,
-#         "memory": 50000
-#     }
-# CONTAINER_PROPERTIES
-# }
+    type = "container"
+    container_properties = <<CONTAINER_PROPERTIES
+{
+        "image": "${var.account_id}.dkr.ecr.eu-west-1.amazonaws.com/${each.key}",
+        "executionRoleArn": "${var.execution_role_arn}",
+        "jobRoleArn": "${var.job_role_arn}",
+        "vcpus": 24,
+        "memory": 50000
+    }
+CONTAINER_PROPERTIES
+}
 
 
-# resource "aws_batch_job_definition" "batch_job_definition_memory" {
+resource "aws_batch_job_definition" "batch_job_definition_memory" {
 
-#     for_each = var.image_names_memory
-#     name = "${each.key}-${var.memory_suffix}"
+    for_each = var.image_names_memory
+    name = "${each.key}-${var.memory_suffix}"
 
-#     tags = {
-#         Terraform   = true
-#         Owner       = var.owner
-#         Name        = each.key
-#     }  
+    tags = {
+        Terraform   = true
+        Owner       = var.owner
+        Name        = each.key
+    }  
   
-#     type = "container"
-#     container_properties = <<CONTAINER_PROPERTIES
-# {
-#         "image": "${var.account_id}.dkr.ecr.eu-west-1.amazonaws.com/${each.key}",
-#         "executionRoleArn": "${var.execution_role_arn}",
-#         "jobRoleArn": "${var.job_role_arn}",
-#         "vcpus": 4,
-#         "memory": 32768
-#     }
-# CONTAINER_PROPERTIES
-# }
+    type = "container"
+    container_properties = <<CONTAINER_PROPERTIES
+{
+        "image": "${var.account_id}.dkr.ecr.eu-west-1.amazonaws.com/${each.key}",
+        "executionRoleArn": "${var.execution_role_arn}",
+        "jobRoleArn": "${var.job_role_arn}",
+        "vcpus": 4,
+        "memory": 32768
+    }
+CONTAINER_PROPERTIES
+}
